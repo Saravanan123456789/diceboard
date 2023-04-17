f203fe4f9a79411289d8270a699a31260f4a5a7f
Saravanan123456789 <smsaran1724@gmail.com>
2023-04-17 10:39:22 +0530

    initial commit Done.

---
+++ src/components/diceboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./diceboard.css"
+
+const diceboard =({face})=> {
+    return 
+        <i className={'fas fa-dice-${faace}'}/>;
+
+    };
+export default diceboard
---
+++ src/components/spindice.js
@@ -0,0 +1,12 @@
+import React from "react";
+import diceboard from "./diceboard";
+import "./spindice.css";
+const spindice = () =>{
+    return(
+        <>
+        <diceboard face="one"/>
+        </>
+    );
+};
+
+export default spindice;

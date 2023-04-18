24b231933faca350a8623de1cd3105c4e9770ace
Saravanan123456789 <smsaran1724@gmail.com>
2023-04-18 09:50:02 +0530

    updated my style sheet

--- src/components/Diceboard.jsx
+++ src/components/Diceboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import './Diceboard.css'
 function Diceboard()
   {
   const [rolledValue, setRolledValue] = useState(1);
@@ -9,11 +9,15 @@ function Diceboard()
   };
 
   return (
+    <div>
+    
     <div className="app">
+    
+      <h1>Dice Board</h1>
      <button onClick={roll} className="rollingdice">roll</button>
       <p>rolled dice value: {rolledValue}</p>
     </div>
-    
+    </div>
   );
 }
 
